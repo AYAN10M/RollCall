5e8edc46b1186260be324bf7cbbbd28cfcfaeb1f
AYAN10M <gopalchandrahaldar1997@gmail.com>
2024-09-01 19:47:33 +0530

    Basic Structure Done

---
+++ attendance_application/lib/dashboard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyDashboard extends StatelessWidget {
+  const MyDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Dashboard'),
+      ),
+    );
+  }
+}
