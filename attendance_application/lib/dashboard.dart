70f0c8e229f8df3b3d53c46389a33e3b93ee2888
AYAN10M <gopalchandrahaldar1997@gmail.com>
2024-09-01 21:14:19 +0530

    Switch Added

--- attendance_application/lib/dashboard.dart
+++ attendance_application/lib/dashboard.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 
-class MyDashboard extends StatelessWidget {
+class MyDashboard extends StatefulWidget {
   const MyDashboard({super.key});
 
   @override
+  State<MyDashboard> createState() => _MyDashboardState();
+}
+
+class _MyDashboardState extends State<MyDashboard> {
+  bool isSwitched = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Dashboard'),
+        child: Transform.scale(
+          scale: 2,
+          child: Switch(
+              activeColor: Colors.deepPurple,
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                  // take the switch output from here
+                  print(isSwitched);
+                });
+              }),
+        ),
       ),
     );
   }
