abb86fe16642b768e8ba865aedd33a658f97de67
Akash Sharma <akashsharma91795@gmail.com>
2024-09-03 21:56:30 +0530

    Location access solved

--- attendance_application/lib/dashboard.dart
+++ attendance_application/lib/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MyDashboard extends StatefulWidget {
   const MyDashboard({super.key});
@@ -9,6 +10,35 @@ class MyDashboard extends StatefulWidget {
 
 class _MyDashboardState extends State<MyDashboard> {
   bool isSwitched = false;
+
+  Future<Position> getCurrentLocation() async {
+
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    LocationPermission permission;
+
+    if (!serviceEnabled) {
+
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+      
+      
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +53,13 @@ class _MyDashboardState extends State<MyDashboard> {
                 onChanged: (value) {
                   setState(() {
                     isSwitched = value;
+                    getCurrentLocation().then((value) {
+                      print(value);
+                    })
+                    .catchError((error) {
+                      print(error);
+                    });
+
                     // take the switch output from here
                     print(isSwitched);
                   });
